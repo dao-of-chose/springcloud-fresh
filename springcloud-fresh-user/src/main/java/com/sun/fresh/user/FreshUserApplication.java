d07c6c1a2a292accb31c6120aef0ef0e04560037
sun <1076748669@qq.com>
2020-05-17 16:27:06 +0800

    添加读取配置中心

---
+++ springcloud-fresh-user/src/main/java/com/sun/fresh/user/FreshUserApplication.java
@@ -0,0 +1,15 @@
+package com.sun.fresh.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@SpringBootApplication
+@EnableEurekaClient			//将应用注册到eureka服务
+@EnableRedisHttpSession    //将session缓存到redis中
+public class FreshUserApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FreshUserApplication.class, args);
+	}
+}
